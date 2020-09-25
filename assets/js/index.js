d0ab65b9f44893d5519a2d20b76c044c11fc50d8
Emilyn Pieritz <66131506+EPieritz@users.noreply.github.com>
2020-09-25 19:10:49 -0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,204 @@
+let inputSearch = null;
+let checkBoxLanguages = null;
+let radioButtonConditionals = null;
+let filterConditional = null;
+let filterText = "";
+let formSearch = null;
+let preloader = null;
+let pnlSearch = null;
+let panelDevs = null;
+let devs = [];
+let filteredLanguages = [];
+
+window.addEventListener("load", async () => {
+  getElements();
+  await fetchDevs();
+  addEvent();
+});
+
+function getElements() {
+  inputSearch = document.querySelector("#search");
+  checkBoxLanguages = document.querySelectorAll("[name='languesTypes']");
+  radioButtonConditionals = document.querySelectorAll(
+    "[name='conditionalTypes']"
+  );
+  filterConditional = document.querySelector(
+    "[name='conditionalTypes']:checked"
+  ).id;
+  checkBoxLanguages.forEach((language) =>
+    language.checked ? filteredLanguages.push(language.id) : null
+  );
+  formSearch = document.querySelector("#frmSearch");
+  preloader = document.querySelector("#preloader");
+  pnlSearch = document.querySelector("#pnlSearch");
+  panelDevs = document.querySelector("#panelDevs");
+}
+
+async function fetchDevs() {
+  const rest = await fetch("http://localhost:3001/devs");
+  const json = await rest.json();
+  devs = json
+    .map(({ id, name, picture, programmingLanguages }) => {
+      const nameLowerCase = name
+        .toLocaleLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f-\x20]/g, "");
+      return {
+        id,
+        name,
+        nameLowerCase,
+        picture,
+        programmingLanguages,
+      };
+    })
+    .sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+
+  devs = devs.map(
+    ({ id, name, picture, nameLowerCase, programmingLanguages }) => {
+      const programmingLanguagesWithIcons = [];
+      const languagesTypes = [];
+      programmingLanguages.forEach((languages) => {
+        const { language, experience, id } = languages;
+        const idLowerCase = id.toLocaleLowerCase();
+        languagesTypes.push(language.toLocaleLowerCase());
+        switch (language) {
+          case "Java":
+            programmingLanguagesWithIcons.push({
+              idLowerCase,
+              language,
+              experience,
+              icon: "./assets/img/java.png",
+            });
+            break;
+          case "JavaScript":
+            programmingLanguagesWithIcons.push({
+              idLowerCase,
+              language,
+              experience,
+              icon: "./assets/img/javascript.png",
+            });
+            break;
+          case "Python":
+            programmingLanguagesWithIcons.push({
+              idLowerCase,
+              language,
+              experience,
+              icon: "./assets/img/python.png",
+            });
+            break;
+          default:
+            break;
+        }
+      });
+      return {
+        id,
+        name,
+        nameLowerCase,
+        picture,
+        languagesTypes /*: languagesTypes.join("").toLocaleLowerCase(),*/,
+        programmingLanguagesWithIcons,
+      };
+    }
+  );
+
+  //console.log(devs);
+  showDevs(devs);
+  showPreloader();
+}
+
+function showPreloader() {
+  setTimeout(() => {
+    preloader.classList.add("hidden");
+    pnlSearch.classList.remove("hidden");
+    activateInput();
+  }, 2000);
+}
+
+function addEvent() {
+  inputSearch.addEventListener("input", handleKeyUp);
+  checkBoxLanguages.forEach((checkBoxLanguage) => {
+    checkBoxLanguage.addEventListener("input", handleSelectedLanguage);
+  });
+  radioButtonConditionals.forEach((radioButtonConditional) => {
+    radioButtonConditional.addEventListener("input", handleSelectedConditional);
+  });
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+}
+
+function activateInput() {
+  inputSearch.focus();
+}
+
+function handleKeyUp(event) {
+  filterText = event.target.value;
+  filterDevs();
+}
+
+function handleSelectedLanguage() {
+  filteredLanguages = [];
+  const filterLanguages = document.querySelectorAll(
+    "[name=languesTypes]:checked"
+  );
+  filterLanguages.forEach(({ id }) => {
+    filteredLanguages.push(id);
+  });
+  filterDevs();
+}
+
+function handleSelectedConditional(event) {
+  filterConditional = event.target.id;
+  filterDevs();
+}
+
+function filterDevs() {
+  let filteredDevs = [];
+  const filterTextLowerCase = filterText.toLocaleLowerCase();
+  filteredDevs = devs.filter((dev) => {
+    return dev.nameLowerCase.includes(filterTextLowerCase);
+  });
+  filteredDevs = filteredDevs.filter((dev) => {
+    return filterConditional === "or"
+      ? filteredLanguages.some((item) => dev.languagesTypes.includes(item))
+      : dev.languagesTypes.join("") === filteredLanguages.join("");
+  });
+  showDevs(filteredDevs);
+}
+
+function showDevs(devs) {
+  panelDevs.innerHTML = "";
+  if (devs === null) {
+    // quando nao tem nenhum registro de devs
+    const h6 = document.createElement("h6");
+    h6.innerHTML = "No registered/found 'dev'";
+    panelDevs.appendChild(h6);
+  } else {
+    // encontrou registro de devs
+    const h3 = document.createElement("h5");
+    h3.innerHTML = `${devs.length} registered/found 'devs'`;
+    panelDevs.appendChild(h3);
+    const ul = document.createElement("ul");
+    devs.forEach((dev) => {
+      const { picture, name, programmingLanguagesWithIcons } = dev;
+      const li = document.createElement("li");
+      li.classList.add("flex-row");
+      li.classList.add("space-bottom");
+      li.classList.add("col");
+      li.classList.add("s4");
+      li.classList.add("panel");
+      const img = `<img class='avatar' src="${picture}" alt="${name}" />`;
+      const userData = `<span>${name}</span>`;
+      let languages = "";
+      programmingLanguagesWithIcons.forEach(({ icon, language }) => {
+        languages = `${languages} <img class='language-icon' src="${icon}" alt="${language}"></img>`;
+      });
+      li.innerHTML = `${img}${userData}${languages}`;
+      ul.appendChild(li);
+    });
+    panelDevs.appendChild(ul);
+  }
+}
